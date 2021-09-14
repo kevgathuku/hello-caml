13ed4b7c27b7fb7a6ca1e3fec01b79bf2a97ebda
Kevin Gathuku <kevgathuku@gmail.com>
2021-09-14 22:33:08 +0300

    Add example using external library - graphics

--- helloworld.ml
+++ helloworld.ml
@@ -1,1 +1,11 @@
-let () = print_endline "Hello, World!"
+Random.self_init ();;
+
+Graphics.open_graph " 640x480";;
+
+for i = 12 downto 1 do
+  let radius = i * 20 in
+    Graphics.set_color (if i mod 2 = 0 then Graphics.red else Graphics.yellow);
+    Graphics.fill_circle 320 240 radius
+done;;
+
+read_line ();;
