0a1ee11462e1b268a782c8786cc6acda12ea5505
Kevin Gathuku <kevgathuku@gmail.com>
2021-09-14 22:35:38 +0300

    Add example with opening a module

--- helloworld.ml
+++ helloworld.ml
@@ -1,11 +1,11 @@
-Random.self_init ();;
+open Graphics;;
 
-Graphics.open_graph " 640x480";;
+open_graph " 640x480";;
 
 for i = 12 downto 1 do
   let radius = i * 20 in
-    Graphics.set_color (if i mod 2 = 0 then Graphics.red else Graphics.yellow);
-    Graphics.fill_circle 320 240 radius
+    set_color (if i mod 2 = 0 then red else yellow);
+    fill_circle 320 240 radius
 done;;
 
 read_line ();;
